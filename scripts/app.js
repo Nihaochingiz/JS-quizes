eacaac1f07d0b0df961ebc4e15b852a62ed247ec
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-05-16 11:06:23 +0300

    Added fields and methods, added Result class

--- scripts/app.js
+++ scripts/app.js
@@ -2,43 +2,84 @@ const headElem = document.getElementById("head");
 const buttonsElem = document.getElementById("buttons");
 const pagesElem = document.getElementById("pages");
 
-
 class Quiz
 {
-	constructor(type, questions)
+	constructor(type, questions, results)
 	{
+		//1 или 2
 		this.type = type;
 		this.questions = questions;
+		this.results = results;
+		this.score = 0;
+
+		this.current = 0;
+	}
+
+	Click(index)
+	{
+		this.score += this.questions[this.current].Click(index);
+		this.Next();
+	}
+
+	Next()
+	{
+		this.current++;
+		
+		if(this.current >= this.questions.length) 
+		{
+			this.End();
+		}
+	}
+
+	End()
+	{
+		for(var i = 0; i < this.results.length; i++)
+		{
+
+		}
 	}
 } 
 
 class Question 
 {
-	constructor(text, questions)
+	constructor(text, answers)
 	{
-		this.text = text;
-		this.questions = questions;
+		this.text = text; 
+		this.answers = answers; 
+	}
+
+	Click(index) 
+	{
+		return this.answers[index].value; 
 	}
 }
 
 class Answer 
 {
-	constructor(text, value, isCorrect = false) 
+	constructor(text, value) 
+	{
+		this.text = text; 
+		this.value = value; 
+	}
+}
+
+class Result 
+{
+	constructor(text, value)
 	{
 		this.text = text;
 		this.value = value;
-		this.isCorrect = isCorrect;
 	}
 
-	Click(type)
+	Check(value)
 	{
-		if(type == 1)
+		if(this.value <= value)
 		{
-			return this.isCorrect;
+			return true;
 		}
-		else
+		else 
 		{
-			return this.value;
+			return false;
 		}
 	}
 }
