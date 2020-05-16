1c64d1d25cf5e35f901ac73f45088ddbf362405c
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-05-16 10:48:10 +0300

    Added Quiz, Question and Answer classes

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,42 @@ const buttonsElem = document.getElementById("buttons");
 const pagesElem = document.getElementById("pages");
 
 
+class Quiz
+{
+	constructor(type, questions)
+	{
+		this.type = type;
+		this.questions = questions;
+	}
+} 
+
+class Question 
+{
+	constructor(text, questions)
+	{
+		this.text = text;
+		this.questions = questions;
+	}
+}
+
+class Answer 
+{
+	constructor(text, value, isCorrect = false) 
+	{
+		this.text = text;
+		this.value = value;
+		this.isCorrect = isCorrect;
+	}
+
+	Click(type)
+	{
+		if(type == 1)
+		{
+			return this.isCorrect;
+		}
+		else
+		{
+			return this.value;
+		}
+	}
+}
